693399839247ba9056953136c49b049b054f7eb1
goodbyh <greatguy.bae@gmail.com>
2017-01-19 22:12:16 +0900

    Add server files

---
+++ blogproject/blogproject/src/main/java/com/b2ks/BlogprojectApplication.java
@@ -0,0 +1,12 @@
+package com.b2ks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogprojectApplication.class, args);
+	}
+}
