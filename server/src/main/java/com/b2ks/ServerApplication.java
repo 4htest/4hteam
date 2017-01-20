d0d589f81e584fad98a9fe1c8af938065b752a8d
goodbyh <greatguy.bae@gmail.com>
2017-01-21 06:40:23 +0900

    Fix server files

--- server/src/main/java/com/b2ks/ServerApplication.java
+++ server/src/main/java/com/b2ks/ServerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogprojectApplication {
+public class ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlogprojectApplication.class, args);
+		SpringApplication.run(ServerApplication.class, args);
 	}
 }
