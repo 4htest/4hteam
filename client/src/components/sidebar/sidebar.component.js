183ea657185d3f4fb0a5beb2886c2f94ac40945b
KwonSH <suhy2002@naver.com>
2017-02-06 22:04:34 +0900

    Add(reactsidebar)
    reactsidebar

--- client/src/components/sidebar/sidebar.component.js
+++ client/src/components/sidebar/sidebar.component.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import styles from './sidebar.component.css';
+
+import { SideNav, Nav } from 'react-sidenav';
  
 class SideBarComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { selected: 'dashboard' }
+        this.onSelection= this.onSelection.bind(this);
+    }
+
+    onSelection(selection) {
+        this.setState({ selected: selection.id });
+        //or trigger a dispatch here 
+    }	
+
     render(){
-        return (
-        	<div className={styles.sidebar}>
-        	</div>
+		let navi = [
+		    { id: 'dashboard', icon: 'fa fa-dashboard' , text: 'Dashboard'},
+		    { id: 'products', icon: 'fa fa-cube', text: 'Products' ,
+		        navlist: [
+		          { icon: 'fa fa-desktop', id: 'manage' ,text: 'Manage Product' },
+		          { icon: 'fa fa-cog', id: 'suppliers' ,text: 'Suppliers' }
+		        ]
+		    },
+		    { id: 'inventory', icon: 'fa fa-database' ,text: 'Inventory'},
+		    { id: 'deliveries', icon: 'fa fa-truck' ,text: 'Deliveries'},
+		    { id: 'reports', icon: 'fa fa-bar-chart' ,text: 'Reports' }
+		];
+
+        return ( 
+	        	<div className={styles.sidebar}>
+					<SideNav selected={this.state.selected} navs={navi} onSelection={this.onSelection}/>
+	        	</div>
         );
     }
 }
