5b38b6f9dfefdc3bcd8da08beb156d79042b3d3e
gkswns04 <gkswns04@naver.com>
2017-02-15 17:15:52 +0900

    ADD(reply reducer)

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import post from './post/post.reducer';
+import reply from './reply/reply.reducer';
 import { combineReducers } from 'redux';
 
 export default combineReducers({
-	post
+	post,
+	reply
 });
---
+++ client/src/reducers/reply/reply.reducer.js
@@ -0,0 +1,6 @@
+import * as types from '../../actions/ActionTypes';
+import update from 'react-addons-update';
+
+const initialState = {
+    value: 0
+};
