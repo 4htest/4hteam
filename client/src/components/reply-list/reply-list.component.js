7839ef6436d5240a6e7f51ec32407425ad437b7c
gkswns04 <gkswns04@naver.com>
2017-02-16 19:29:09 +0900

    UPDATE(reply CRUD)

--- client/src/components/reply-list/reply-list.component.js
+++ client/src/components/reply-list/reply-list.component.js
@@ -5,21 +5,64 @@ import Reply from '../../components/reply/reply.component';
 export default class ReplyListComponent extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			value:'',
+			data: this.props.data
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+  		this.handleSubmit = this.handleSubmit.bind(this);
+
 	}
 
+	handleChange(event) {
+	    this.setState({value: event.target.value});
+	}
+
+  	handleSubmit(event) {
+	    alert('A content was submitted: ' + this.state.value);
+	    event.preventDefault();
+	}
+
+	delete(item) {
+    	const newState = this.state.data;
+  		newState.splice(newState.indexOf(item), 1);
+  		this.setState({data: newState});
+  	}
+
+  	update(item) {
+  		const newState = this.state.data;
+  		console.log(newState[newState.indexOf(item)]);
+  		newState[newState.indexOf(item)].content = 'hi';
+  		this.setState({data: newState});
+  	}
+
+
 	render() {
-		const test = data => {
+		const item = data => {
 			return data.map(i => {
 				return (
-					<Reply key={i.toString()} index={i} />
+					<div key={i.id}>
+						<Reply content={i.content}/>
+						<button onClick={this.delete.bind(this, i)}>Delete</button>
+						<button onClick={this.update.bind(this, i)}>Update</button>
+					</div>
 				);
 			});
 		};
 
 		return (
-			<div className={styles.replyList}>
-				{test([1,2,3,4,5])}
+			<div>{item(this.state.data)}
+				<form onSubmit={this.handleSubmit}>
+			        <label>
+			          content:
+			          <textarea value={this.state.value} onChange={this.handleChange} />
+			        </label>
+			        <input type="submit" value="Submit" />
+		     	</form>
+
 			</div>
+			
 		);
 	}
 }
--- client/src/reducers/reply/reply.reducer.js
+++ client/src/reducers/reply/reply.reducer.js
@@ -2,5 +2,61 @@ import * as types from '../../actions/ActionTypes';
 import update from 'react-addons-update';
 
 const initialState = {
-    value: 0
+    data: [
+            {id: '1', content: 'reply 1'},
+            {id: '2', content: 'reply 2'},
+            {id: '3', content: 'reply 3'},
+            {id: '4', content: 'reply 4'},
+            {id: '5', content: 'reply 5'}
+        ]
 };
+
+export default function reply(state, action) {
+    if(typeof state === "undefined") {
+        state = initialState;
+    }
+
+    switch(action.type) {
+    case types.POST_LIST:
+        return update(state, {
+            list: {
+                status: { $set: 'WAITING' },
+            }
+        });
+    case types.POST_LIST_SUCCESS: 
+        if(action.isInitial) {
+            return update(state, {
+                list: {
+                    status: { $set: 'SUCCESS' },
+                    data: { $set: action.data },
+                    // isLast: { $set: action.data.length < 6 }
+                }
+            })
+        } else {
+            if(action.listType === 'new') {
+                return update(state, {
+                    list: {
+                        status: { $set: 'SUCCESS' },
+                        data: { $unshift: action.data },
+                    }
+                });
+            } else {
+                return update(state, {
+                    list: {
+                        status: { $set: 'SUCCESS' },
+                        data: { $push: action.data },
+                        // isLast: { $set: action.data.length < 6 }
+                    }
+                });    
+            }
+        }
+    case types.POST_LIST_FAILURE:
+        return update(state, {
+            list: {
+                status: { $set: 'FAILURE' }
+            }
+        })
+    default:
+            return state;
+    }
+}
