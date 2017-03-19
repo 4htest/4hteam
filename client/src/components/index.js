bfcee2574b9dba28cfcdb3a8636f22736a794eb1
gkswns04 <gkswns04@naver.com>
2017-03-19 17:02:21 +0900

    ADD(dropdown component)

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -7,7 +7,8 @@ import ReplyListComponent from './reply-list/reply-list.component';
 import CategoryListComponent from './category-list/category-list.component';
 import CategoryComponent from './category/category.component';
 import CategoryCreateComponent from './category-create/category-create.component';
+import DropdownComponent from './dropdown/dropdown.component';
 
 export { HeaderComponent, SideBarComponent, PostingComponent, 
 	ReplyComponent, ReplyListComponent, ReplyCreateComponent, CategoryListComponent, CategoryComponent,
-	CategoryCreateComponent };
+	CategoryCreateComponent, DropdownComponent };
